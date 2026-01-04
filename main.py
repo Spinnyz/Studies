2684d83aa78dea640f4d2f9e3b8ca2381b98a99a
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-04 15:08:21 -0300

    up

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-texto = "Python"
- 
-novo_texto = ""
-for i in texto:
-    novo_texto += f"* {i}"
-print(novo_texto)
+for i in range(10):
+    if i == 2:
+        print ("i é 2")
+        continue
+
+    elif i == 8:
+        print ("i é 8")
+        continue
+
+    for j in range (1,3):
+        print (i,j)
+
+else:
+    print ("fim")
