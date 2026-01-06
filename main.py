1ecc058a8747a4db49cdaf3ea36c2acf169fa65a
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-06 16:38:29 -0300

    Split

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-lista = []
+frase = "Olha que,  legal"
 
-while True:
-    user = input ("[i]inserir [a]apagar [l]listar: ").lower().strip()
+separador = (frase.split(",")) 
 
-    if user == "i":
-        valor = input ("Oque deseja inserir: ")
-        lista.append(valor)
+for i, frase in enumerate(separador):
+    separador[i] = separador[i].strip()
 
-    elif user == "a":
-        remove = input("Oque deseja remover: ")
-        if remove in lista:
-            print ("Removido")
-            lista.remove(remove)
-        else:
-            print ("Não encontrado")
-    
-    elif user == "l":
-        for indice, item in enumerate(lista):
-            print (indice, item)
-    else:
-        print ("Escolha uma opção valida")
+print(separador)
