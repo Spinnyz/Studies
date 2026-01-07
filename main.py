da863542b7a1d39bbb88853a048ffb53898215e0
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-07 19:37:11 -0300

    criar uma função que soma dois números e mostra o resultado.

--- main.py
+++ main.py
@@ -1,56 +1,6 @@
-import re
+#criar uma função que soma dois números e mostra o resultado.
 
-cpf = re.sub (
-    r"[^0-9]",
-    "",
-    "74682489070")
+def soma (a,b):
+    print (a+b)
 
-# ---------- Primeiro dígito ----------
-nove_digitos = cpf[:9]
-
-contador_regressivo = 10
-soma = 0
-
-for i in nove_digitos:
-    valor = int(i) * contador_regressivo
-    soma += valor
-    contador_regressivo -= 1
-
-print(soma)
-
-digito_1 = (soma * 10) % 11
-
-if digito_1 > 9:
-    digito_1 = 0
-else:
-    print(digito_1)
-
-
-# ---------- Segundo dígito ----------
-dez_digitos = cpf[:10]
-segundo_contador_regressivo = 11
-soma = 0
-
-for i in dez_digitos:
-    segundo_valor = int(i) * segundo_contador_regressivo
-    soma += segundo_valor
-    segundo_contador_regressivo -= 1
-
-print(soma)
-
-digito_2 = (soma * 10) % 11
-
-if digito_2 > 9:
-    digito_2 = 0
-else:
-    print(digito_2)
-
-
-# ---------- Verificação ----------
-novo_cpf = f"{nove_digitos}{digito_1}{digito_2}"
-print(novo_cpf)
-
-if novo_cpf == cpf:
-    print("CPF válido")
-else:
-    print("CPF inválido")
+soma(1,2)
