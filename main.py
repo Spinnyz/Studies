8b752d14d260d3c5571718737a32e3208377cabe
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-07 13:54:46 -0300

    up

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ nove_digitos = cpf[:9]
 
 contador_regressivo = 10
 soma = 0
-
+resultado = 0
 for i in nove_digitos:
     valor = int(i) * contador_regressivo
     soma += valor
-    x -= 1
-    print (valor, end=" ")
+    contador_regressivo -= 1
+    resultado +=valor
+
+print(resultado)
+
+resto =  ((resultado * 10) % 11)
+
+if resto > 9:
+    print ("O resultado é 0")
+
+else:
+    print (i)
+
