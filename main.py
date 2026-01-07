7432d32b3ae8cecb737cb12dcbe09c9b07c9293b
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-07 13:57:58 -0300

    Primeiro digito de um CPF

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 cpf = "57316421096"
 
+# primeiro digito
 nove_digitos = cpf[:9]
 
 contador_regressivo = 10
 soma = 0
-resultado = 0
 for i in nove_digitos:
     valor = int(i) * contador_regressivo
     soma += valor
     contador_regressivo -= 1
-    resultado +=valor
 
-print(resultado)
+print(soma)
 
-resto =  ((resultado * 10) % 11)
+digito_1 =  ((soma * 10) % 11)
 
-if resto > 9:
+if digito_1 > 9:
     print ("O resultado é 0")
 
 else:
