d6e0880443bc35f876276066d40017b66d452eba
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-09 14:09:36 -0300

    Up

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-d1 = {
-    "c1": 10,
-    "c2": 20,
-    "c3": 30,
+perguntas = [{
+    "pergunta": "Quanto é 2+2?",
+    "opcoes": ["1", "3", "4", "5"],
+    "resposta": "4"
+},
+{
+  "pergunta": "Quanto é 5*6?",
+  "opcoes": ["11", "30", "56", "60"],
+  "resposta": "30"
+},
+{
+  "pergunta": "Quanto é 10/2?",
+  "opcoes": ["2", "5", "10", "20"],
+  "resposta": "5"  
 }
+]
 
-d2 = d1.copy()
-d2["c1"]=1000
-print(d1)
-print(d2) 
+for pergunta in perguntas:
+    print("Pergunta:", pergunta["pergunta"])
+    print()
+
+
+    for i, opcao in enumerate(pergunta["opcoes"]):
+        print(f"{i}) {opcao}")
+        print()
