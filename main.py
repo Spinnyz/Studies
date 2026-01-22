6f0fb3e6c301ec55e6ca2bf764843bb93cbfc370
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-01-21 21:52:10 -0300

    up

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-lista = [
-    {"nome":"Pedro"}
-    
-]
+class Node:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esq = None
+        self.dir = None
+
+
+def inverter_arvore(no):
+    if no is None:
+        return None
+
+    # troca os filhos
+    no.esq, no.dir = no.dir, no.esq
+
+    # inverte recursivamente
+    inverter_arvore(no.esq)
+    inverter_arvore(no.dir)
+
+    return no
+
+
+print (inverter_arvore(raiz))
