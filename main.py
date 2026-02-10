7d009c83b7060ab4ba17cc6d6d7bd09860aaa005
Pedro Rodrigues <pedrolucasgonza31@gmail.com>
2026-02-10 19:41:10 -0300

    Filtro

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-lista = [ 2 for number in range (1,11)
+
+
+produtos = [
+    {"nome": "Camiseta", "preco": 29.90},
+    {"nome": "Calça", "preco": 99.90},
+    {"nome": "Tênis", "preco": 149.90},
 ]
 
-
-print(lista)
+novos_produtos = [
+   {**produto, "preco": produto["preco"] * 1.05} 
+   if produto["preco"] > 20 else produto
+   for produto in produtos
+]
+print(novos_produtos)
